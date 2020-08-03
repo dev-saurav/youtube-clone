8c0bde20c5d99a154d2df9d79f7503912fd52763
webDevSaurav <webdevelopersaurav@gmail.com>
2020-08-03 20:33:11 +0530

    Updates

---
+++ precache-manifest.e0aef1c16d7e07d2415b443c098c8efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1de1a2c23bb7be198878a1ef0aecd6",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "c5ea881957f60b41ac38",
+    "url": "/youtube-clone/static/css/main.ddd21cea.chunk.css"
+  },
+  {
+    "revision": "a0744025c362061b7d23",
+    "url": "/youtube-clone/static/js/2.aa7b638a.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtube-clone/static/js/2.aa7b638a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ea881957f60b41ac38",
+    "url": "/youtube-clone/static/js/main.0268e471.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
